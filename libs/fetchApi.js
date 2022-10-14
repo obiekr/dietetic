bdc217de149456215748b5585caf8b724495e181
obiekr <obiekrisnanto1@gmail.com>
2022-10-15 01:54:48 +0700

    fix food page banner

--- libs/fetchApi.js
+++ libs/fetchApi.js
@@ -1,11 +1,5 @@
 import qs from 'qs';    
 
-/** 
- * @param {string} path path of URL
- * @param {string} paramsObj Object of params 
- * @param {string} options Object of options
- */
-
  async function fetchAPI(path, paramsObj={}, options={}){
     const {headers, ...option} = options
     const mergedOption = {
@@ -17,7 +11,7 @@ import qs from 'qs';
     }
 
     const queryStr = qs.stringify(paramsObj)
-    const reqURL = ` https://api.edamam.com/api${path}${queryStr? `?${queryStr}` : ""}`
+    const reqURL = `https://api.edamam.com/api${path}${queryStr? `?${queryStr}` : ""}`
 
     const res = await fetch(reqURL, mergedOption)
 
--- pages/[food].jsx
+++ pages/[food].jsx
@@ -9,46 +9,47 @@ export default function food(a) {
             <div className="max-w-full mb-10 px-1 py-10 bg-[#D0E0FF] ">
                 <h1 className="text-center font-bold">Breakfast</h1>
             </div>
-            <div className="w-full">
-                <div className="flex flex-col justify-center mx-40 relatve">
-                    <h1 className='flex-auto font-semibold mx-80'>Nama Makanan</h1>
-                    <div className="bg-[#D0E0FF] flex justify-between items-center max-w-5xl">
-                        <div className="mx-5 w-auto text-center ml-96">
+            <div className="">
+                <h1 className='mx-auto font-semibold text-center'>Nama Makanan</h1>
+
+                <div className="flex justify-center max-w-full">
+                    <div className="bg-[#D0E0FF] flex justify-between items-center relative py-5 px-56">
+                        <div className="mx-6 w-auto text-center ml-[20%]">
                             <div className='font-semibold text-sm text-slate-500'>Cooking Time:</div>
                             <div className='text-sm text-slate-400'>35 Minutes</div>
                         </div>
-                        <div className="mx-5 w-auto text-center">
+                        <div className="mx-6 w-auto text-center">
                             <div className='font-semibold text-sm text-slate-500'>Total Calories:</div>
                             <div className='text-sm text-slate-400'>300 kcal</div>
                         </div>
-                        <div className="mx-5 w-auto text-center">
+                        <div className="mx-6 w-auto text-center">
                             <div className='font-semibold text-sm text-slate-500'>Fat:</div>
                             <div className='text-sm text-slate-400'>20g</div>
                         </div>
-                        <div className="mx-5 w-auto text-center">
+                        <div className="mx-6 w-auto text-center">
                             <div className='font-semibold text-sm text-slate-500'>Carbs:</div>
                             <div className='text-sm text-slate-400'>20g</div>
                         </div>
-                        <div className="mx-5 w-auto text-center">
+                        <div className="mx-6 w-auto text-center">
                             <div className='font-semibold text-sm text-slate-500'>Protein:</div>
                             <div className='text-sm text-slate-400'>20g</div>
                         </div>
-                        <div className="w-11 overflow-hidden inline-block">
-                            <div className="h-16 bg-white -rotate-45 transform origin-top-right"></div>
+                        <div className="overflow-hidden absolute right-0 z-50 translate-y-1">
+                            <div className="py-20 px-14 bg-white -rotate-45 transform origin-top-right"></div>
+                        </div>
+                        <div className="w-96 absolute -left-20 -top-16">
+                            <Image src={foodPic} alt="" layout='responsive' className='' />
                         </div>
                     </div>
                 </div>
-                <div className="w-96 absolute left-40">
-                    <Image src={foodPic} alt="" layout='responsive' className='' />
-                </div>
+
             </div>
 
-            <div className="flex mx-auto justify-center">
+            <div className="flex justify-center">
 
 
-                <div className="flex-wrap">
 
-
+                <div className="translate-x-24">
                     <h2 className='font-black text-xl mt-10 ml-10'>Ingredients:</h2>
                     <div className="text-md mt-2 ml-11 font-light text-stone-500">1. xxxxxxxxxxx</div>
                     <div className="text-md ml-11 font-light text-stone-500">2. xxxxxxxxxxx</div>
@@ -58,7 +59,9 @@ export default function food(a) {
 
                     <h2 className='font-black text-xl mt-10 ml-10'>Directions:</h2>
                     <div className="text-md mt-5 ml-10 font-semibold">Step 1</div>
-                    <div className="text-md mt-1 ml-10 font-light text-stone-500">xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</div>
+                    <div className="text-md mt-1 ml-10 font-light text-stone-500">
+                        xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+                    </div>
                 </div>
             </div>
         </div>
